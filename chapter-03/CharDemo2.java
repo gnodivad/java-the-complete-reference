59d3b9334730bdca8e6d113bbfbdf35b5f84554f
David <dodwei.dev@gmail.com>
2019-01-20 13:11:21 +0800

    Add source code for C03L04

---
+++ chapter-03/CharDemo2.java
@@ -0,0 +1,17 @@
+/**
+ * C03 L04
+ * 
+ * char variables behave like integers.
+ */
+public class CharDemo2 {
+
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
+
+        ch1++;
+        System.out.println("ch1 is now " + ch1);
+    }
+}
