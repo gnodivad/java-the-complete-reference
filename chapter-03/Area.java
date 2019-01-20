0f057994e7a3d5aacf2a3e5ed0d7cbe6e824137a
David <dodwei.dev@gmail.com>
2019-01-20 13:03:13 +0800

    Add source code for C03L02

---
+++ chapter-03/Area.java
@@ -0,0 +1,17 @@
+/**
+ * C03 L02
+ * 
+ * Compute the area of a circle.
+ */
+public class Area {
+
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8;
+        pi = 3.1416;
+        a = pi * r * r;
+
+        System.out.println("Area of circle is " + a);
+    }
+}
