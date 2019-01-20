713dabe952b1bc561e06d5f5e0de9664b7b45001
David <dodwei.dev@gmail.com>
2019-01-20 12:07:31 +0800

    Add source code for C02L01

---
+++ chapter-02/Example.java
@@ -0,0 +1,10 @@
+/**
+ * C02 L01
+ * 
+ * This is a simple Java program. Call this file "Example.java"
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+}
