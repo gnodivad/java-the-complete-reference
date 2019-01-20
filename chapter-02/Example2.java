e064e7ed03c253f937b66112d8cd46abee5da562
David <dodwei.dev@gmail.com>
2019-01-20 12:16:22 +0800

    Add source code for C02L02

---
+++ chapter-02/Example2.java
@@ -0,0 +1,19 @@
+/**
+ * C02 L02
+ * 
+ * Here is another short example. Call this file "Example2.java"
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+
+        num = 100;
+
+        System.out.println("This is num: " + num);
+
+        num = num * 2;
+
+        System.out.print("The value of num * 2 is ");
+        System.out.println(num);
+    }
+}
