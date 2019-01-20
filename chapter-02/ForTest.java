445b4f01e2c64c39cf13a7e805e923f57a26cef2
David <dodwei.dev@gmail.com>
2019-01-20 12:28:35 +0800

    Add source code for C02L04

---
+++ chapter-02/ForTest.java
@@ -0,0 +1,14 @@
+/**
+ * C02 L04
+ * 
+ * Demonstrate the for loop. Call this file "ForTest.java".
+ */
+public class ForTest {
+
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x = x + 1)
+            System.out.println("This is x: " + x);
+    }
+}
