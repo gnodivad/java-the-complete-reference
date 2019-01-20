f3e2ad97abc945268dd9ba00398d52c53b85474d
David <dodwei.dev@gmail.com>
2019-01-20 17:48:07 +0800

    Add source code for C03L06

---
+++ chapter-03/DynInit.java
@@ -0,0 +1,15 @@
+/**
+ * C03 L06
+ * 
+ * Demostrate dynamic initialization
+ */
+public class DynInit {
+
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println("Hypotenuse is " + c);
+    }
+}
