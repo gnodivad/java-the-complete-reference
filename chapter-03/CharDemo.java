6fdf43cbd765d8786f92862f19a76b4828b5c5b5
David <dodwei.dev@gmail.com>
2019-01-20 13:07:53 +0800

    Add source code for C03L03

---
+++ chapter-03/CharDemo.java
@@ -0,0 +1,17 @@
+/**
+ * C03 L03
+ * 
+ * Demonstrate char data type.
+ */
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88;
+        ch2 = 'Y';
+
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
