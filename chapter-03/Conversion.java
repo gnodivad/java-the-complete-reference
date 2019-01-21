3ed5d8bb98eae25293a79a7e4231450be7e6a6c7
David <dodwei.dev@gmail.com>
2019-01-21 11:27:02 +0800

    Add source code for C03L09

---
+++ chapter-03/Conversion.java
@@ -0,0 +1,25 @@
+/**
+ * C03 L09
+ * 
+ * Conversion
+ */
+public class Conversion {
+
+    public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.142;
+
+        System.out.println("\nConversion of int to byte.");
+        b = (byte) i;
+        System.out.println("i and b " + i + " " + b);
+
+        System.out.println("\nConversion of double to int.");
+        i = (int) d;
+        System.out.println("d and i " + d + " " + i);
+
+        System.out.println("\nConversion of double to byte.");
+        b = (byte) d;
+        System.out.println("d and b " + d + " " + b);
+    }
+}
