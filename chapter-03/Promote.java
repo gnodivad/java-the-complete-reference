bde174b5a5d1ec89aae0a47ce606871045a3fe86
David <dodwei.dev@gmail.com>
2019-01-21 12:19:13 +0800

    Add source code for C03L10

---
+++ chapter-03/Promote.java
@@ -0,0 +1,19 @@
+/**
+ * C03 L10
+ * 
+ * Promote
+ */
+public class Promote {
+
+    public static void main(String[] args) {
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = .1234;
+        double result = (f * b) + (i / c) - (d * s);
+        System.out.println((f * b) + " + " + (i / c) + " - " + (d * s));
+        System.out.println("result = " + result);
+    }
+}
