794c84ef39e7eb4630be2d82a11f49c9c8520aa2
David <dodwei.dev@gmail.com>
2019-01-21 15:45:15 +0800

    Add source code for C03L12

---
+++ chapter-03/AutoArray.java
@@ -0,0 +1,12 @@
+/**
+ * C03 L12
+ * 
+ * An improved version of the previous program.
+ */
+public class AutoArray {
+
+    public static void main(String[] args) {
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        System.out.println("April has " + month_days[12] + " days.");
+    }
+}
