5633505d69824532469bcc9306ce3a8d1eee74a7
David <dodwei.dev@gmail.com>
2019-01-21 15:59:17 +0800

    Add source code for C03L14

---
+++ chapter-03/TwoDArray.java
@@ -0,0 +1,26 @@
+/**
+ * C03 L14
+ * 
+ * Demonstrate a two-dimensional array.
+ */
+public class TwoDArray {
+
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][5];
+        int i, j, k = 0;
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                twoD[i][j] = k;
+                k++;
+            }
+        }
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                System.out.print(twoD[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
