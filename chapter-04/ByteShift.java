cec54313e5ef56008aef6b7dc3e955f727018302
David <dodwei.dev@gmail.com>
2019-01-22 16:24:56 +0800

    Add source code for C04L06

---
+++ chapter-04/ByteShift.java
@@ -0,0 +1,18 @@
+/**
+ * C04 L06
+ * 
+ * Left shifting a byte value.
+ */
+public class ByteShift {
+
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("Original value of a: " + a);
+        System.out.println("i and b: " + i + " " + b)
+    }
+}
