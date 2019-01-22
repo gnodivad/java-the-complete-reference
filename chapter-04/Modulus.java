cbf0e4769df690686c269a524a154a25c83dee62
David <dodwei.dev@gmail.com>
2019-01-22 15:53:08 +0800

    Add source code for C04L02

---
+++ chapter-04/Modulus.java
@@ -0,0 +1,15 @@
+/**
+ * C04 L02
+ * 
+ * Demonstrate the % operator.
+ */
+public class Modulus {
+
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
