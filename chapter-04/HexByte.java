3c8ef539a08ecacfc88e72a291b0b8d3ae955202
David <dodwei.dev@gmail.com>
2019-01-22 16:33:00 +0800

    Add source code for C04L08

---
+++ chapter-04/HexByte.java
@@ -0,0 +1,14 @@
+/**
+ * C04 L08
+ * 
+ * Masking sign extension.
+ */
+public class HexByte {
+
+    public static void main(String[] args) {
+        char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+        byte b = (byte) 0xf1;
+        System.out.println("b = 0x" + hex[(b >> 4) & 0x0f] + hex[b & 0x0f]);
+    }
+}
