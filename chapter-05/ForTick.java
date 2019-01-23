7031137a0f25a386146aa3cbd568c86084ecd4e1
David <dodwei.dev@gmail.com>
2019-01-23 16:36:06 +0800

    Add source code for C05L10

---
+++ chapter-05/ForTick.java
@@ -0,0 +1,15 @@
+/**
+ * C05 L10
+ * 
+ * Demonstrate the for loop.
+ */
+public class ForTick {
+
+    public static void main(String[] args) {
+        int n;
+
+        for (n = 10; n > 0; n--) {
+            System.out.println("tick " + n);
+        }
+    }
+}
