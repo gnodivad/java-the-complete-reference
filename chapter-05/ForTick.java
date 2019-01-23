4c03039de0e1f7d08ab457134ebe2953255b7d5f
David <dodwei.dev@gmail.com>
2019-01-23 16:36:56 +0800

    Add source code for C05L10

--- chapter-05/ForTick.java
+++ chapter-05/ForTick.java
@@ -6,9 +6,7 @@
 public class ForTick {
 
     public static void main(String[] args) {
-        int n;
-
-        for (n = 10; n > 0; n--) {
+        for (int n = 10; n > 0; n--) {
             System.out.println("tick " + n);
         }
     }
