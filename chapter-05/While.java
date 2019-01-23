424613deb614e9bce4e40ea5c3ecbf3e9fbfb612
David <dodwei.dev@gmail.com>
2019-01-23 15:57:14 +0800

    Add source code for C05L06

---
+++ chapter-05/While.java
@@ -0,0 +1,16 @@
+/**
+ * C05 L06
+ * 
+ * Demonstrate the while loop.
+ */
+public class While {
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        while (n > 0) {
+            System.out.println("tick " + n);
+            n--;
+        }
+    }
+}
