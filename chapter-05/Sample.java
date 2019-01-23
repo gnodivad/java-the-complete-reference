1f615b8e69a906572abe9538d17ba3b3ad48faf0
David <dodwei.dev@gmail.com>
2019-01-23 16:42:12 +0800

    Add source code for C05L12

---
+++ chapter-05/Sample.java
@@ -0,0 +1,18 @@
+/**
+ * C05 L12
+ * 
+ * Sample
+ */
+public class Sample {
+
+    public static void main(String[] args) {
+        int a, b;
+
+        b = 4;
+        for (a = 1; a < b; a++) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            b--;
+        }
+    }
+}
