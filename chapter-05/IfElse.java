235638eb26fb8e73659af7e425783150f3182997
David <dodwei.dev@gmail.com>
2019-01-23 15:29:18 +0800

    Add source code for C05L01

---
+++ chapter-05/IfElse.java
@@ -0,0 +1,26 @@
+/**
+ * C05 L01
+ * 
+ * Demonstrate if-else-if statements.
+ */
+public class IfElse {
+
+    public static void main(String[] args) {
+        int month = 4;
+        String season;
+
+        if (month == 12 || month == 1 || month == 2) {
+            season = "Winter";
+        } else if (month == 3 || month == 4 || month == 5) {
+            season = "Spring";
+        } else if (month == 6 || month == 7 || month == 8) {
+            season = "Summer";
+        } else if (month == 9 || month == 10 || month == 11) {
+            season = "Autumn";
+        } else {
+            season = "Bogus Month";
+        }
+
+        System.out.println("April is in the " + season + ".");
+    }
+}
