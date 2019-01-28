43bd834d35e19b8a4a9608178b2d6ef2c07d5f66
David <dodwei.dev@gmail.com>
2019-01-28 13:15:26 +0800

    Add source code for C06L01

---
+++ chapter-06/BoxDemo.java
@@ -0,0 +1,27 @@
+/**
+ * C06 L01
+ * 
+ */
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+/**
+ * This class declares an object of type Box.
+ */
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box mybox = new Box();
+        double vol;
+
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+
+        vol = mybox.width * mybox.height * mybox.depth;
+
+        System.out.println("Volume is " + vol);
+    }
+}
