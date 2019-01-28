90e1b944b82e540398f19d12988327ac72f8e191
David <dodwei.dev@gmail.com>
2019-01-28 16:27:31 +0800

    Add source code for C07L04

--- chapter-07/OverloadCons2.java
+++ chapter-07/OverloadCons2.java
@@ -1,21 +1,32 @@
-class Box {
+/**
+ * C07 L04
+ * 
+ * Box allows one object to initialize another.
+ */
+class Box2 {
     double width;
     double height;
     double depth;
 
-    Box(double w, double h, double d) {
+    Box2(Box2 ob) {
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
+
+    Box2(double w, double h, double d) {
         width = w;
         height = h;
         depth = d;
     }
 
-    Box() {
+    Box2() {
         width = -1;
         height = -1;
         depth = -1;
     }
 
-    Box(double len) {
+    Box2(double len) {
         width = height = depth = len;
     }
 
@@ -24,11 +35,13 @@ class Box {
     }
 }
 
-public class OverloadCons {
+public class OverloadCons2 {
     public static void main(String[] args) {
-        Box mybox1 = new Box(10, 20, 15);
-        Box mybox2 = new Box();
-        Box mycube = new Box(7);
+        Box2 mybox1 = new Box2(10, 20, 15);
+        Box2 mybox2 = new Box2();
+        Box2 mycube = new Box2(7);
+
+        Box2 myclone = new Box2(mybox1);
 
         double vol;
 
@@ -40,6 +53,9 @@ public class OverloadCons {
 
         vol = mycube.volume();
         System.out.println("Volume of mycube is " + vol);
+
+        vol = myclone.volume();
+        System.out.println("Volume of myclone is " + vol);
     }
 
 }
