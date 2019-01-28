3640bb00b55c0ebc56af9a6067abdf1b2a3d29a8
David <dodwei.dev@gmail.com>
2019-01-28 16:08:57 +0800

    Add source code for C07L02

---
+++ chapter-07/OverloadCons.java
@@ -0,0 +1,45 @@
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    Box() {
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    Box(double len) {
+        width = height = depth = len;
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+public class OverloadCons {
+    public static void main(String[] args) {
+        Box mybox1 = new Box(10, 20, 15);
+        Box mybox2 = new Box();
+        Box mybox3 = new Box(7);
+
+        double vol;
+
+        vol = mybox1.volume();
+        System.out.println("Volume of mybox1 is " + vol);
+
+        vol = mybox2.volume();
+        System.out.println("Volume of mybox2 is " + vol);
+
+        vol = mybox3.volume();
+        System.out.println("Volume of mybox3 is " + vol);
+    }
+
+}
