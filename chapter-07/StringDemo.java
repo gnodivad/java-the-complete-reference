5f63b72da84c6b8c8a31b071816cab5375db6364
David <dodwei.dev@gmail.com>
2019-01-29 16:46:25 +0800

    Add source code for C07L18

---
+++ chapter-07/StringDemo.java
@@ -0,0 +1,17 @@
+/**
+ * C07 L18
+ * 
+ * Demonstrating strings.
+ */
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String strOb1 = "First String";
+        String strOb2 = "Second String";
+        String strOb3 = strOb1 + " and " + strOb2;
+
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strOb3);
+    }
+}
