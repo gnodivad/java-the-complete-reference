799bf5f9da02b68ce8c7ef9b44c4aa178cec0c56
David <dodwei.dev@gmail.com>
2019-01-29 16:52:00 +0800

    Add source code for C07L20

---
+++ chapter-07/StringDemo3.java
@@ -0,0 +1,15 @@
+/**
+ * C07 L20
+ * 
+ * Demonstrate String arrays
+ */
+public class StringDemo3 {
+
+    public static void main(String[] args) {
+        String str[] = { "one", "two", "three" };
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.println("str[" + i + "]: " + str[i]);
+        }
+    }
+}
