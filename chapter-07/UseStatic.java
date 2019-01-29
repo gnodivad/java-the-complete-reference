202d7684471f7fe0e92dc12f9bc9bf1e42af1287
David <dodwei.dev@gmail.com>
2019-01-29 15:54:52 +0800

    Add source code for C07L12

---
+++ chapter-07/UseStatic.java
@@ -0,0 +1,24 @@
+/**
+ * C07 L12
+ * 
+ * Demonstrate static variables, methods, and blocks.
+ */
+public class UseStatic {
+    static int a = 3;
+    static int b;
+
+    static void meth(int x) {
+        System.out.println("x = " + x);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    static {
+        System.out.println("Static block initialized.");
+        b = a * 4;
+    }
+
+    public static void main(String args[]) {
+        meth(42);
+    }
+}
