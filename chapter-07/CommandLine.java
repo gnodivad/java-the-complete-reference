5129f0bf2d4fee205e64e193daefbabdfb337b96
David <dodwei.dev@gmail.com>
2019-01-29 16:56:57 +0800

    Add source code for C07L21

---
+++ chapter-07/CommandLine.java
@@ -0,0 +1,12 @@
+/**
+ * C07 L21
+ * 
+ * Display all command-line arguments.
+ */
+public class CommandLine {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+    }
+}
