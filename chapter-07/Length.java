af2e1fcdb8fbc27e2d4b9db01035f3c48eed4a66
David <dodwei.dev@gmail.com>
2019-01-29 16:02:11 +0800

    Add source code for C07L14

---
+++ chapter-07/Length.java
@@ -0,0 +1,17 @@
+/**
+ * C07 L14
+ * 
+ * This program demonstrates the length array member.
+ */
+public class Length {
+
+    public static void main(String[] args) {
+        int a1[] = new int[10];
+        int a2[] = { 3, 5, 7, 1, 8, 99, 44, -10 };
+        int a3[] = { 4, 3, 2, 1 };
+
+        System.out.println("length of a1 is " + a1.length);
+        System.out.println("length of a2 is " + a2.length);
+        System.out.println("length of a3 is " + a3.length);
+    }
+}
