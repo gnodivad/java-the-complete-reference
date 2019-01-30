b9184fcc76b7d22e4115baa5b666e2c8e3ba8e5b
David <dodwei.dev@gmail.com>
2019-01-30 08:59:16 +0800

    Add source code for C07L22

---
+++ chapter-07/PassArray.java
@@ -0,0 +1,25 @@
+/**
+ * C07 L22
+ * 
+ * Use an array to pass a variable number of arguments to a method. This is the
+ * old-style approach to variable-length arguments
+ */
+public class PassArray {
+    static void vaTest(int v[]) {
+        System.out.print("Number of args: " + v.length + " Contents: ");
+        for (int x : v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n1[] = { 10 };
+        int n2[] = { 1, 2, 3 };
+        int n3[] = { 10 };
+
+        vaTest(n1);
+        vaTest(n2);
+        vaTest(n3);
+    }
+}
