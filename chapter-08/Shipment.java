f0dc9f5f7f8c7937fccc02cef32b4884b2d3f00e
David <dodwei.dev@gmail.com>
2019-01-31 18:33:28 +0800

    Add source code for C08L07

---
+++ chapter-08/Shipment.java
@@ -0,0 +1,23 @@
+public class Shipment extends BoxWeight {
+    double cost;
+
+    Shipment(Shipment ob) {
+        super(ob);
+        cost = ob.cost;
+    }
+
+    Shipment(double w, double h, double d, double m, double c) {
+        super(w, h, d, m);
+        cost = c;
+    }
+
+    Shipment() {
+        super();
+        cost = -1;
+    }
+
+    Shipment(double len, double m, double c) {
+        super(len, m);
+        cost = c;
+    }
+}
