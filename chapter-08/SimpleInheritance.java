e103a6f5dd6151e251005f714cae86cdcd0a9dea
David <dodwei.dev@gmail.com>
2019-01-31 17:52:24 +0800

    Add source code for C08L01

---
+++ chapter-08/SimpleInheritance.java
@@ -0,0 +1,49 @@
+/**
+ * C08 L01
+ * 
+ * A simple example of inheritance.
+ */
+class A {
+    int i, j;
+
+    void showij() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
+
+class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+
+    void sum() {
+        System.out.println("i+j+k: " + (i + j + k));
+    }
+}
+
+public class SimpleInheritance {
+
+    public static void main(String[] args) {
+        A superOb = new A();
+        B subOb = new B();
+
+        superOb.i = 10;
+        superOb.j = 20;
+        System.out.println("Contents of superOb: ");
+        superOb.showij();
+        System.out.println();
+
+        subOb.i = 7;
+        subOb.j = 8;
+        subOb.k = 9;
+        System.out.println("Contents of subOb: ");
+        subOb.showij();
+        subOb.showk();
+        System.out.println();
+
+        System.out.println("Sum of i, j and k in subOb:");
+        subOb.sum();
+    }
+}
