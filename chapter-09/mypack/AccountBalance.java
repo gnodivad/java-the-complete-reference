851a83b217ab91b3855d18d27691e39a0faf8f5b
David <dodwei.dev@gmail.com>
2019-02-01 16:56:51 +0800

    Add source code for C09L01

---
+++ chapter-09/mypack/AccountBalance.java
@@ -0,0 +1,37 @@
+/**
+ * C09 L01
+ * 
+ * A simple package
+ */
+package mypack;
+
+class Balance {
+    String name;
+    double bal;
+
+    Balance(String n, double b) {
+        name = n;
+        bal = b;
+    }
+
+    void show() {
+        if (bal < 0) {
+            System.out.print("--> ");
+        }
+        System.out.println(name + ": $" + bal);
+    }
+}
+
+public class AccountBalance {
+
+    public static void main(String[] args) {
+        Balance current[] = new Balance[3];
+
+        current[0] = new Balance("K. J. Fielding", 123.23);
+        current[1] = new Balance("Will Tell", 157.02);
+        current[2] = new Balance("Tom Jackson", -12.33);
+
+        for (int i = 0; i < 3; i++)
+            current[i].show();
+    }
+}
