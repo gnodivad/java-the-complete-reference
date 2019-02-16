c08a849a1a0ed2d78101a134a897856830f9dc18
David <dodwei.dev@gmail.com>
2019-02-16 17:24:40 +0800

    Add source code for C19L08

---
+++ chapter-19/IteratorDemo.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+/**
+ * C19 L08
+ * 
+ * Demonstrate iterators.
+ */
+public class IteratorDemo {
+
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+
+        System.out.print("Original contents of al: ");
+        Iterator<String> itr = al.iterator();
+        while (itr.hasNext()) {
+            String element = itr.next();
+            System.out.print(element + " ");
+        }
+        System.out.println();
+
+        ListIterator<String> litr = al.listIterator();
+        while (litr.hasNext()) {
+            String element = litr.next();
+            litr.set(element + "+");
+        }
+
+        System.out.print("Modified contents of al: ");
+        itr = al.iterator();
+        while (itr.hasNext()) {
+            String element = itr.next();
+            System.out.print(element + " ");
+        }
+        System.out.println();
+
+        System.out.print("Modified list backwards: ");
+        while (litr.hasPrevious()) {
+            String element = litr.previous();
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
