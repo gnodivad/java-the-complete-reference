72313f1c38f118199343252a99015395603fa7e6
David <dodwei.dev@gmail.com>
2019-02-17 14:33:02 +0800

    Add source code for C19L13

---
+++ chapter-19/TreeMapDemo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/**
+ * C19 L13
+ */
+public class TreeMapDemo {
+
+    public static void main(String[] args) {
+        TreeMap<String, Double> tm = new TreeMap<>();
+
+        tm.put("John Doe", 3434.34);
+        tm.put("Tom Smith", 123.22);
+        tm.put("Jane Baker", 1378.00);
+        tm.put("Tod Hall", 99.22);
+        tm.put("Ralph Smith", -19.08);
+
+        Set<Map.Entry<String, Double>> set = tm.entrySet();
+
+        for (Map.Entry<String, Double> me : set) {
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+        System.out.println();
+
+        double balance = tm.get("John Doe");
+        tm.put("John Doe", balance + 1000);
+
+        System.out.println("John Doe's new balance: " + tm.get("John Doe"));
+    }
+}
