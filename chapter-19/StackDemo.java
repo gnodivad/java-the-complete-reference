1ae33b7d1892261b00612187590d27566348e2de
David <dodwei.dev@gmail.com>
2019-02-17 16:36:24 +0800

    Add source code for C19L21

---
+++ chapter-19/StackDemo.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+/**
+ * C19 L21
+ * 
+ * Demonstrate the Stack class.
+ */
+public class StackDemo {
+    static void showpush(Stack<Integer> st, int a) {
+        st.push(a);
+        System.out.println("push(" + a + ")");
+        System.out.println("stack: " + st);
+    }
+
+    static void showpop(Stack<Integer> st) {
+        System.out.print("pop -> ");
+        Integer a = st.pop();
+        System.out.println(a);
+        System.out.println("stack: " + st);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<Integer>();
+
+        System.out.println("stack: " + st);
+        showpush(st, 42);
+        showpush(st, 66);
+        showpush(st, 99);
+        showpop(st);
+        showpop(st);
+        showpop(st);
+
+        try {
+            showpop(st);
+        } catch (EmptyStackException e) {
+            System.out.println("empty stack");
+        }
+    }
+}
