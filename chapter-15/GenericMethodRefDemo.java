ccb9dbcc2779bedee47e4468ccf14e6291c64772
David <dodwei.dev@gmail.com>
2019-02-27 16:41:41 +0800

    Add source code for C15L13

---
+++ chapter-15/GenericMethodRefDemo.java
@@ -0,0 +1,40 @@
+/**
+ * C15 L13
+ * 
+ * Demonstrate a method reference to a generic method declared inside a
+ * non-generic class.
+ */
+interface MyFunc3<T> {
+    int func(T[] vals, T v);
+}
+
+class MyArrayOps {
+    static <T> int countMatching(T[] vals, T v) {
+        int count = 0;
+
+        for (int i = 0; i < vals.length; i++)
+            if (vals[i] == v) {
+                count++;
+            }
+
+        return count;
+    }
+}
+
+public class GenericMethodRefDemo {
+    static <T> int myOp(MyFunc3<T> f, T[] vals, T v) {
+        return f.func(vals, v);
+    }
+
+    public static void main(String[] args) {
+        Integer[] vals = { 1, 2, 3, 4, 2, 3, 4, 4, 5 };
+        String[] strs = { "One", "Two", "Three", "Two" };
+        int count;
+
+        count = myOp(MyArrayOps::<Integer>countMatching, vals, 4);
+        System.out.println("vals contains " + count + " 4s");
+
+        count = myOp(MyArrayOps::<String>countMatching, strs, "Two");
+        System.out.println("strs contains " + count + " Twos");
+    }
+}
